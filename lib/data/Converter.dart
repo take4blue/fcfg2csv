b231504bf66c5f6f6b9e710d34bf50b40eeec474
take4 <46077789+take4blue@users.noreply.github.com>
2022-02-21 10:41:12 +0900

    変換本体完了

---
+++ lib/data/Converter.dart
@@ -0,0 +1,113 @@
+import 'dart:typed_data';
+import 'package:flutter/foundation.dart';
+
+class Convert extends ChangeNotifier {
+  String fcfgParameter = "";
+  String csvParameter = "";
+
+  void toCsvParamere() {
+    csvParameter = convert(fcfgParameter);
+    notifyListeners();
+  }
+
+  // FlashPrintのパラメータ形式をCSV形式に変換する
+  static String convert(String value) {
+    // 数値の後方0を取り除くもの
+    RegExp trailingZeroSuppress = RegExp(r'([.]*0+)(?!.*\d)');
+
+    // 変換処理本体
+    var lines = value.split('\n');
+    var result = "";
+    lines.forEach((String line) {
+      // 1行分の処理
+      // =がない場合はそのまま出力。=がある場合は=を,に置き換えて@Variant内のエスケープ文字を32ビット浮動小数点に変換して出力する
+      var equalPos = line.indexOf('=');
+      if (equalPos == -1) {
+        result += line + '\n';
+      } else {
+        var words = <String>[];
+        words.add(line.substring(0, equalPos));
+        words.add(line.substring(equalPos + 1));
+        result += words[0] + ',';
+        if (words[1].indexOf("@Variant") >= 0) {
+          // ()内のエスケープ文字を取り出して32ビット浮動小数点に変換し出力
+          var start = words[1].indexOf('(') + 1;
+          var end = words[1].indexOf(')');
+          var view = escapeStringToDouble(words[1].substring(start, end));
+          result +=
+              view.toStringAsFixed(3).replaceAll(trailingZeroSuppress, '');
+        } else if (words[1].indexOf('[') >= 0) {
+          result += '"' + words[1] + '"';
+        } else {
+          result += words[1];
+        }
+        result += '\n';
+      }
+    });
+    return result;
+  }
+
+  // 文字が16進数の文字かどうか判断し、もしそうなら数値化して返す
+  static int toHexDigit(String s) {
+    var ch = s.codeUnitAt(0);
+    if ((ch ^ 0x30) <= 9) {
+      return ch ^ 0x30;
+    } else if ((ch ^ 0x40) <= 6 && (ch ^ 0x40) > 0) {
+      return (ch ^ 0x40) + 9;
+    } else if ((ch ^ 0x60) <= 6 && (ch ^ 0x60) > 0) {
+      return (ch ^ 0x60) + 9;
+    } else {
+      return -1;
+    }
+  }
+
+  // エスケープされた文字列とASCII文字列をUint8List形式に変換する
+  static Uint8List unescapeString(String string) {
+    var stringLength = string.length;
+    var result = <int>[];
+    for (var i = 0; i < stringLength; i++) {
+      if (string[i] == r'\') {
+        i++;
+        if (i >= stringLength) {
+          break;
+        }
+        if (string[i] == '0') {
+          result.add(0x00);
+        } else if (string[i] == 'f') {
+          result.add(0x0C);
+        } else if (string[i] == 'a') {
+          result.add(0x07);
+        } else if (string[i] == 'r') {
+          result.add(0x0D);
+        } else if (string[i] == 'x') {
+          i++;
+          if (i >= stringLength) {
+            break;
+          }
+          var val = toHexDigit(string[i]);
+          if (i + 1 < stringLength) {
+            var nextnext = toHexDigit(string[i + 1]);
+            if (nextnext >= 0) {
+              i++;
+              val = val * 16 + nextnext;
+            }
+          }
+          result.add(val);
+        }
+      } else {
+        result.add(string.codeUnitAt(i));
+      }
+    }
+    return Uint8List.fromList(result);
+  }
+
+  // エスケープされたASCII文字列を32ビット浮動小数点として変換する
+  static double escapeStringToDouble(String str) {
+    var bytes = unescapeString(str);
+    if (bytes.length == 8) {
+      return ByteData.sublistView(bytes).getFloat32(4);
+    } else {
+      return double.nan;
+    }
+  }
+}
---
+++ test/converter_text.dart
@@ -0,0 +1,72 @@
+import 'package:fcfg2csv/data/Converter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  var epsilon = 0.0001;
+  test('toHexDigit', () {
+    expect(Convert.toHexDigit("0"), 0);
+    expect(Convert.toHexDigit("9"), 9);
+    expect(Convert.toHexDigit("A"), 10);
+    expect(Convert.toHexDigit("F"), 15);
+    expect(Convert.toHexDigit("a"), 10);
+    expect(Convert.toHexDigit("f"), 15);
+    expect(Convert.toHexDigit("@"), -1);
+    expect(Convert.toHexDigit("G"), -1);
+    expect(Convert.toHexDigit("/"), -1);
+    expect(Convert.toHexDigit(":"), -1);
+    expect(Convert.toHexDigit("`"), -1);
+    expect(Convert.toHexDigit("g"), -1);
+  });
+
+  test('unescapeString', () {
+    expect(Convert.unescapeString("a"), equals(<int>[0x61]));
+    expect(Convert.unescapeString(r"\"), equals(<int>[]));
+    expect(Convert.unescapeString(r"\0"), equals(<int>[0x00]));
+    expect(Convert.unescapeString(r"\f"), equals(<int>[0x0C]));
+    expect(Convert.unescapeString(r"\a"), equals(<int>[0x07]));
+    expect(Convert.unescapeString(r"\r"), equals(<int>[0x0D]));
+    expect(Convert.unescapeString(r"\x1"), equals(<int>[0x01]));
+    expect(Convert.unescapeString(r"\x01"), equals(<int>[0x01]));
+    expect(Convert.unescapeString(r"\x10"), equals(<int>[0x10]));
+    expect(Convert.unescapeString(r"\x00"), equals(<int>[0x00]));
+    expect(Convert.unescapeString(r"\xFF"), equals(<int>[0xff]));
+    expect(Convert.unescapeString(r"\xff"), equals(<int>[0xff]));
+    expect(Convert.unescapeString(r"\x1\xFF"), equals(<int>[0x01, 0xff]));
+    expect(Convert.unescapeString(r"\x1\0"), equals(<int>[0x01, 0x00]));
+    expect(Convert.unescapeString(r"\x1u"), equals(<int>[0x01, 0x75]));
+  });
+
+  test('escapeStringToDouble', () {
+    expect(Convert.escapeStringToDouble('').isNaN, isTrue);
+    expect(Convert.escapeStringToDouble('0000000').isNaN, isTrue);
+    expect(Convert.escapeStringToDouble('000000000').isNaN, isTrue);
+    expect(Convert.escapeStringToDouble(r'\0\0\0\0\0\0\0\0'), equals(0.0));
+    expect(Convert.escapeStringToDouble(r'\0\0\0\x87?\xe0\0\0'),
+        closeTo(1.75, epsilon));
+    expect(Convert.escapeStringToDouble(r'\0\0\0\x87>\xcc\xcc\xcd'),
+        closeTo(0.4, epsilon));
+    expect(Convert.escapeStringToDouble(r'\0\0\0\x87\x43\x66\0\0'),
+        closeTo(230.0, epsilon));
+    expect(Convert.escapeStringToDouble(r'\0\0\0\x87>\xcc\xcc\xcd'),
+        closeTo(0.4, epsilon));
+    expect(Convert.escapeStringToDouble(r'\0\0\0\x87>\x5\x1e\xb8'),
+        closeTo(0.13, epsilon));
+  });
+
+  test('convert', () {
+    expect(Convert.convert(r"""[Custom]
+machineId=9
+nozzleSize=@Variant(\0\0\0\x87>\xcc\xcc\xcd)
+[General]
+extruderTemp0=@Variant(\0\0\0\x87\x43R\0\0)
+baseSpeed="@Variant(\0\0\0\x87\x42p\0\0)"
+fillLayerDensitys=[]"""), equals('''[Custom]
+machineId,9
+nozzleSize,0.4
+[General]
+extruderTemp0,210
+baseSpeed,60
+fillLayerDensitys,"[]"
+'''));
+  });
+}
